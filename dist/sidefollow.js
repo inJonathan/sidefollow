6de7c5155aa66d47c03de8198ab34940d58c203a
Jonathan <jonrcao@gmail.com>
2016-12-15 14:52:28 +0800

    add proj

---
+++ dist/sidefollow.js
@@ -0,0 +1,29 @@
+/*
+ *   name: SideFollow
+ * author: Jonathan
+ *   date: 2016-12-15
+ *   site: caozhuo.net
+ *
+ */
+
+(function($) {
+    $.fn.sideFollow = function(dtime) {
+
+        var floating = $(this);
+        var originalTop = parseInt($(this).css('top'));
+
+        dtime ? dtime = dtime : dtime = 1000;
+
+        // 滚动条监控
+        $(window).scroll(function() {
+            var scrollTop = $(this).scrollTop();
+            floating.animate({
+                top: originalTop + scrollTop
+            }, {
+                duration: dtime,
+                queue: false // 动画不进入动画队列
+            });
+        }); 
+
+    }
+})(jQuery);
