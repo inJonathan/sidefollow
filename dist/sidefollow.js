4a2706214946d17f84142e105d828e60f3b12e7f
Jonathan <jonrcao@gmail.com>
2016-12-19 18:20:07 +0800

    copy to dist

--- dist/sidefollow.js
+++ dist/sidefollow.js
@@ -7,12 +7,16 @@
  */
 
 (function($) {
-    $.fn.sideFollow = function(dtime) {
+    $.fn.sideFollow = function(options) {
 
         var floating = $(this);
         var originalTop = parseInt($(this).css('top'));
 
-        dtime ? dtime = dtime : dtime = 1000;
+        var dft = { //以下为该插件的属性及其默认值
+            time: 1000
+        };
+
+        var ops = $.extend(dft, options);  // 可使用 options 替换默认参数
 
         goFollow(); // 页面加载后先执行一次
 
@@ -26,10 +30,10 @@
             floating.animate({
                 top: originalTop + scrollTop
             }, {
-                duration: dtime,
+                duration: ops.time,
                 queue: false // 动画不进入动画队列
             });
         }
 
-    }
+    };
 })(jQuery);
